d5eea77365ea1c97c4c9c6d51670c5794c7cb85a
anishsaini <anishoficial@gmail.com>
2021-07-30 11:17:22 +0530

    commit

---
+++ weather.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import requests
+import time
+
+
+def getWeather(canvas):
+    city = textField.get()
+    api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
+
+    json_data = requests.get(api).json()
+    condition = json_data['weather'][0]['main']
+    temp = int(json_data['main']['temp'] - 273.15)
+    min_temp = int(json_data['main']['temp_min'] - 273.15)
+    max_temp = int(json_data['main']['temp_max'] - 273.15)
